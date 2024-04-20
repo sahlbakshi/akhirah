afeb56abbc1140244e79e57a3c8a8886518198a1
sahlbakshi <sahlaajazbakshi@gmail.com>
2024-04-20 03:07:28 -0400

    second commit

--- app/page.js
+++ app/page.js
@@ -19,12 +19,12 @@ export default function Home() {
   }
 
   return (
-    <div className="flex flex-col items-center sm:mt-28 mt-10">
-      <div className="text-2xl sm:text-3xl font-bold mb-28">AKHIRAH ATHLETIC</div>
-      <div className={`text-[20px] sm:text-[28px] font-semibold ${message != "" ? "mb-10" : "mb-14"}`}>WAITLIST FOR OUR DROP</div>
+    <div className="flex flex-col items-center sm:mt-28 mt-14">
+      <div className="text-2xl sm:text-3xl font-extrabold mb-24 sm:mb-28">AKHIRAH ATHLETIC</div>
+      <div className={`text-[24px] sm:text-[28px] font-semibold ${message != "" ? "mb-10" : "mb-14"}`}>WAITLIST FOR OUR DROP</div>
       {message != "" ? <div className="w-64 text-center bg-red-100 text-red-900 font-semibold py-6">{message}</div> : null}
       <form className="flex flex-col items-center">
-        <input className="outline-none text-center w-64 bg-black py-4 border-b border-gray-500 font-medium placeholder:font-medium placeholder:text-gray-300 mb-8"
+        <input className="outline-none text-center w-64 bg-black py-4 rounded-none border-b border-gray-500 font-medium placeholder:font-medium placeholder:text-gray-300 mb-8"
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
@@ -39,7 +39,7 @@ export default function Home() {
           <img className="w-4" src="/images/rightarrow.png"></img>
         </button> 
       </form>
-      <div className="mb-28 text-xs">*waitlist gets early access email</div>
+      <div className="mb-24 sm:mb-28 text-xs">*waitlist gets early access email</div>
       <div className="flex gap-2 items-center">
         <Link href=""><img className="w-6" src="/images/instagram.png"></img></Link>
         <Link href=""><img className="w-6" src="/images/tiktok.png"></img></Link>
--- app/success/page.js
+++ app/success/page.js
@@ -4,12 +4,12 @@ import Link from "next/link"
 export default function Success() {
 
   return (
-    <div className="flex flex-col items-center mt-28">
-      <div className="text-3xl font-bold mb-28">AKHIRAH ATHLETIC</div>
+    <div className="flex flex-col items-center sm:mt-28 mt-14">
+      <div className="text-3xl font-extrabold mb-24 sm:mb-28">AKHIRAH ATHLETIC</div>
       <div className="text-[28px] font-semibold mb-14">WAITLIST FOR OUR DROP</div>
       <div className="text-md w-64 text-center font-medium">Thank you for signing up!</div>
       <div className="text-md w-64 text-center font-medium mb-14">Keep an eye out at your mailbox to get notified.</div>
-      <div className="mb-28 text-xs">*waitlist gets early access email</div>
+      <div className="mb-24 sm:mb-28 text-xs">*waitlist gets early access email</div>
       <div className="flex gap-2 items-center">
         <Link href=""><img className="w-6" src="/images/instagram.png"></img></Link>
         <Link href=""><img className="w-6" src="/images/tiktok.png"></img></Link>
