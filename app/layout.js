5d990c507e7e28798fcd3de0f16733f2db1409f1
sahlbakshi <sahlaajazbakshi@gmail.com>
2024-04-20 21:57:00 -0400

    fifth commit

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Akhirah Athletic",
-  description: "",
+  description: "Akhirah Athletic",
 };
 
 export default function RootLayout({ children }) {
